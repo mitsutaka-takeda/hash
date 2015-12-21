fc2f8bc2aaf49ea90dad5b75c16f81dda1948a44
Mitsutaka Takeda <mitsutaka.takeda@gmail.com>
2015-12-22 00:13:45 +0900

    Make integral types contiguously hashable and add tests with rapidcheck

--- hash/hash_traits.hpp
+++ hash/hash_traits.hpp
@@ -7,17 +7,23 @@ namespace hash {
 
 	// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2014/n3980.html
     // A type T is contiguously hashable if for all combinations of two values of a type,
-    // say x and y, if x == y, then it must also be true that memcmp(addressof(x), addressof(y), sizeof(T)) == 0. 
+    // say x and y, if x == y, then it must also be true that memcmp(addressof(x), addressof(y), sizeof(T)) == 0.
     // I.e. if x == y, then x and y have the same bit pattern representation.
-    template <typename T>
+    template <typename, typename = void>
     struct is_contiguously_hashable;
 
     template <typename T>
-    struct is_contiguously_hashable<std::enable_if<std::is_integral<T>::value, T> > : std::true_type {};
+    struct is_contiguously_hashable<T, std::enable_if_t<std::is_integral<T>::value> > : std::true_type {};
+
+    template <typename T>
+    using is_contiguously_hashable_t = typename is_contiguously_hashable<T>::type;
+
+    template <typename T>
+    constexpr bool is_contiguously_hashable_v = is_contiguously_hashable<T>::value;
 
     template <typename HashaAlgorithm, typename T>
     inline
-    std::enable_if_t<hash::is_contiguously_hashable<T>::value>
+    std::enable_if_t<hash::is_contiguously_hashable_v<T> >
     hash_append(HashaAlgorithm& h, T const& t){
         h(std::addressof(t), sizeof(t));
     }
--- hash/hash_traits.t.cpp
+++ hash/hash_traits.t.cpp
@@ -8,10 +8,29 @@
 
 BOOST_AUTO_TEST_SUITE(Integral_is_contiguously_hashable)
 
-RC_BOOST_PROP(int_is_contiguously_hashable, (int a, int b)) {
-    RC_PRE(a == b);
-
-    RC_ASSERT(std::memcmp(std::addressof(a), std::addressof(b), sizeof(int)) == 0);
+namespace  {
+    template <typename Candidate>
+    void
+    check_contiguously_hashability(Candidate x) {
+        static_assert(hash::is_contiguously_hashable_v<Candidate>,
+                      "Candidate must be declared to continuously hashable.");
+        const auto y = x;
+        RC_ASSERT(x == y);
+        RC_ASSERT(std::memcmp(std::addressof(x), std::addressof(y), sizeof(Candidate)) == 0);
+    }
 }
+
+RC_BOOST_PROP(int_is_contiguously_hashable, (int a)) {
+    check_contiguously_hashability(a);
+}
+
+RC_BOOST_PROP(int8_t_is_contiguously_hashable, (int8_t a)) {
+    check_contiguously_hashability(a);
+}
+
+RC_BOOST_PROP(int16_t_is_contiguously_hashable, (int16_t a)) {
+    check_contiguously_hashability(a);
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
