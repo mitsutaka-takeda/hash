731cdfd0ca1fd7197b0939457f99a4ecffb3fb36
Mitsutaka Takeda <mitsutaka.takeda@gmail.com>
2016-01-16 00:39:56 +0900

    Add a fuzzing test with libFuzzer

--- fuzzing/sha_256_fuzzer.cpp
+++ fuzzing/sha_256_fuzzer.cpp
@@ -3,7 +3,6 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size);
 
 extern "C" 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-
     hash::sha_256 h;
     h(data, size);
     return 0;
