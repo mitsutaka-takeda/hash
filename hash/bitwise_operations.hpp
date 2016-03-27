30b5b149e6c0157b3b5fb0b17fb58987797b7f2c
Mitsutaka Takeda <mitsutaka.takeda@gmail.com>
2016-03-28 06:30:18 +0900

    Move the inline function definition out of the class definition
    It's for a better read for class definition, which describes the
    interface.

--- hash/bitwise_operations.hpp
+++ hash/bitwise_operations.hpp
@@ -5,10 +5,18 @@
 
 namespace hash {
     struct bitwise_operations {
+
+        template <typename T>
         static
-        constexpr uint32_t 
-        ROTR(uint8_t n, uint32_t x) noexcept {
-            return (x >> n) | (x << (std::numeric_limits<uint32_t>::digits - n));
-        }
+        constexpr 
+        T
+        ROTR(uint8_t n, T x) noexcept;
     };
 } // namespace hash
+
+template <typename T>
+constexpr 
+T
+hash::bitwise_operations::ROTR(uint8_t n, T x) noexcept {
+    return (x >> n) | (x << (std::numeric_limits<uint32_t>::digits - n));
+}
